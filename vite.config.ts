88b8dcff86c6469701c5431852ef3131cd60cd7d
hiro <571507098@qq.com>
2025-05-15 22:28:07 +0800

    feat:环境配置初始化

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+const pathSrc = resolve(__dirname,"src")
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode,process.cwd())
+  return {
+    resolve:{
+      alias:{
+        "@":pathSrc
+      }
+    },
+    server:{
+      host:"0.0.0.0",
+      port:+env.VITE_APP_PORT,
+      open:true,
+      proxy:{
+        // 代理 /dev-api 请求
+        [env.VITE_APP_BASE_API]:{
+          changeOrigin:true,
+          target:env.VITE_APP_API_URL,
+          rewrite:(path) => path.replace(new RegExp('^' + env.VITE_APP_BASE_API),'')
+        }
+      }
+    },
+    plugins:[
+      vue()
+    ],
+
+  }
 })
