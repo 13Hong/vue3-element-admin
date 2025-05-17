d662c3e6f2f15a2d4921c207326b85a5662dfcb6
hiro <571507098@qq.com>
2025-05-18 01:23:05 +0800

    feat: 更新依赖并配置自动导入和组件解析器

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 
 const pathSrc = resolve(__dirname,"src")
 
@@ -26,7 +30,21 @@ export default defineConfig(({mode}) => {
       }
     },
     plugins:[
-      vue()
+      vue(),
+      AutoImport({
+        imports:["vue"],
+        resolvers:[
+          ElementPlusResolver({ importStyle:"sass" })
+        ],
+        vueTemplate:true,
+        dts:resolve(pathSrc,'types','auto-imports.d.ts') // 自动导入组建类型声明文件
+      }),
+      Components({
+        resolvers:[
+          ElementPlusResolver({ importStyle:'sass' })
+        ],
+        dts:resolve(pathSrc,'types','components.d.ts')
+      })
     ],
 
   }
