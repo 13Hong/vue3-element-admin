7bb55a8ffd35da2c665a76dc7196cc96f9ede761
hiro <571507098@qq.com>
2025-12-03 17:21:59 +0800

    feat: :sparkles: dashboard页面-github角标

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,3 +1,20 @@
+<template>
+  <div class="dashboard-container">
+    <!-- github 角标 -->
+    <github-corner class="github-corner" />
+
+    <el-card shadow="never" class="mt-2">
+      <div class="flex flex-wrap">
+        <!-- 左侧问候语区域 -->
+        <div></div>
+
+        <!-- 右侧图标区域 - PC端 -->
+        <div></div>
+      </div>
+    </el-card>
+  </div>
+</template>
+
 <script setup lang="ts">
 defineOptions({
   name: "Dashboard",
@@ -5,10 +22,17 @@ defineOptions({
 });
 </script>
 
-<template>
-  <div>
-    <h1>Dashboard</h1>
-  </div>
-</template>
+<style lang="scss" scoped>
+.dashboard-container {
+  position: relative;
+  padding: 24px;
 
-<style lang="scss" scoped></style>
+  .github-corner {
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 1;
+    border: 0;
+  }
+}
+</style>
